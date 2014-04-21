7ea2b50a80f803895b6240e412c7296ef6fc5b6d
clashboom <nejeega@gmail.com>
2014-04-21 23:52:08 +0300

    Added mail and simple rate limiting

--- app/js/app.js
+++ app/js/app.js
@@ -9,7 +9,6 @@ var app = (function(document, $) {
 		_init = function() {
 			$(document).foundation();
 			_userAgentInit();
-            console.log('Maybe. Maybe not. Maybe go fuck yourself.');
 		};
 
 	return {
@@ -42,7 +41,64 @@ var app = (function(document, $) {
 
   var listObj = new List('tire-list', options);
 
+  // Calculating shopping cart values
+  if ($('#shopping-cart-total').length) {
 
-console.log('got this far');
+    $(".remove").click(function(){
+      confirm('Tiešām dzēst?');
+    });
+
+    $('.item-count').each(function(){
+        var count = parseInt($(this).val());
+        var price = parseFloat($(this).parent().prev().children('.item-price').text());
+        var sum = (count * price).toFixed(2);
+        $(this).parent().siblings().last('.item-sum').html(sum);
+    });
+
+    var $cart = $("#shopping-cart"),
+    $summands = $cart.find('.item-sum'),
+    $total_sum = $('#shopping-cart-total');
+
+    var sum = 0.0;
+    $summands.each(function(){
+      var value = parseFloat($(this).html());
+      if (!isNaN(value)) sum += value;
+    });
+
+    $('#shopping-cart-total').html(sum.toFixed(2));
+
+    $(".item-count").data("previously", $('.item-count').val());
+    $('.item-count').on('focus', function(){
+      $(this).attr('oldValue', $(this).val());
+    });
+
+    $('.item-count').change(function(){
+
+        var price = parseFloat($(this).parent().prev().children('.item-price').text());
+
+        var previous_val = parseInt($(this).data("previously"));
+        var previous_sum = (price * previous_val);
+
+        var count = parseInt($(this).val());
+        var price = parseFloat($(this).parent().prev().children('.item-price').text());
+        var sum = (count * price);
+        $(this).parent().siblings().last('.item-sum').html(sum.toFixed(2));
+
+        var current_total = parseFloat($('#shopping-cart-total').html());
+        var new_total = (current_total - previous_sum + sum).toFixed(2);
+        $('#shopping-cart-total').html(new_total);
+
+        $(this).data("previously", $(this).val());
+
+    });
+  };
+
+
+
+  // Search bar
+  $('#tyre-search-submit').click(function(){
+    listObj.fuzzySearch.search($('#tyre-search-input').val());
+  });
+
 
 })();
--- app/main.py
+++ app/main.py
@@ -1,24 +1,60 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# Happy Violence!
 import jinja2
 import logging
 import os
-import webapp2
 import urllib
+import webapp2
 
-from google.appengine.runtime import apiproxy_errors
 from google.appengine.api import images
 from google.appengine.api import mail
-from google.appengine.ext import ndb
+from google.appengine.api import memcache
+
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext import ndb
+
+from google.appengine.runtime import apiproxy_errors
+
+from webapp2_extras import sessions
+from webapp2_extras import sessions_memcache
+# from webapp2_extras import sessions_ndb
 
 import collections
+from functools import wraps
 
+
+# Jinja2 Config
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 JINJA_ENV = jinja2.Environment(
     loader=jinja2.FileSystemLoader(TEMPLATE_DIR), autoescape=True)
+
+
+# Webapp2 Sessions config
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'my-super-secret-key',
+    'name': 'bropro_session',
+}
+
+
+def rate_limit(seconds_per_request=1):
+    def rate_limiter(function):
+        @wraps(function)
+        def wrapper(self, *args, **kwargs):
+            added = memcache.add('%s:%s' %
+                                 (self.__class__.__name__,
+                                  self.request.remote_addr or ''), 1,
+                                 time=seconds_per_request,
+                                 namespace='rate_limiting')
+
+            if not added:
+                self.response.write(u'Rate limit exceeded.')
+                self.response.set_status(403)
+                return
+            return function(self, *args, **kwargs)
+        return wrapper
+    return rate_limiter
 
 
 class BaseTyre(ndb.Model):
@@ -36,7 +72,11 @@ class BaseTyre(ndb.Model):
         return cls.query()
 
     @classmethod
-    def toggleVisibility(cls, itemID):
+    def queryTyre(cls, itemID):
+        return cls.get_by_id(int(itemID))
+
+    @classmethod
+    def toggleTyre(cls, itemID):
         tyre = cls.get_by_id(itemID)
         tyre.isHidden = False if tyre.isHidden else True
         # TODO: invalidate memcache or better yet, set in memcache, and add a
@@ -54,7 +94,6 @@ class Tyre(BaseTyre):
     speedIndex = ndb.StringProperty(default=None)
     loadIndex = ndb.StringProperty(default=None)
     image = ndb.BlobProperty(default=None)
-
     inStock = ndb.IntegerProperty(default=0)
 
 
@@ -67,7 +106,7 @@ class UsedCarTyre(CarTyre):
 
 
 class TruckTyre(Tyre):
-    # loadIndexPaired = ndb.StringProperty('lip')
+    # loadIndexPaired = ndb.StringProperty()
     axlePosition = ndb.StringProperty()
 
 
@@ -79,29 +118,118 @@ class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-    tyrePaths = {'CarTyre': 'vieglo',
-                 'UsedCarTyre': 'vieglo/lietotas',
-                 'TruckTyre': 'kravas',
-                 'UsedTruckTyre': 'kravas/lietotas',
-                 'vieglo': CarTyre,
-                 'vieglo/jaunas': CarTyre,
-                 'vieglo/lietotas': UsedCarTyre,
-                 'kravas': TruckTyre,
-                 'kravas/jaunas': TruckTyre,
-                 'kravas/lietotas': UsedTruckTyre}
-
     @classmethod
     def render_str(cls, template, *a, **params):
         template = JINJA_ENV.get_template(template)
         return template.render(params)
 
     def render(self, template, *a, **params):
-        self.write(self.render_str(template, *a, **params))
+        alert = self.session.get('alert')
+        if alert:
+            self.session.pop('alert')
+        self.write(self.render_str(template,
+                                   alert=alert,
+                                   item_count=self.session.get('item_count'),
+                                   *a, **params))
+
+    def dispatch(self):
+        self.session_store = sessions.get_store(request=self.request)
+        try:
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        return self.session_store.get_session(name='bropro_session',
+                                              factory=sessions_memcache.
+                                              MemcacheSessionFactory)
 
 
 class MainHandler(Handler):
     def get(self):
         self.render("home.html")
+
+
+class AddToCartHandler(Handler):
+    def get(self):
+        self.response.headers['Contenty-type'] = 'application/json'
+
+        current_item_count = self.session.get('item_count')
+
+        if not current_item_count:
+            current_item_count = 0
+        current_item_count = int(current_item_count) + 1
+        key = self.request.get('key')
+        item_list = self.session.get('item_list')
+        if not item_list:
+            item_list = []
+        item_list.append(key)
+        self.session['item_list'] = item_list
+        self.session['item_count'] = current_item_count
+        self.redirect('/grozs')
+
+
+class ShowCartHandler(Handler):
+    def get(self, modal=None):
+        item_list = self.session.get('item_list')
+        result = []
+        if item_list:
+            for key_str in item_list:
+                key = ndb.Key(urlsafe=key_str)
+                result.append(key.get())
+        self.render('grozs.html', item_list=result)
+
+    @rate_limit(seconds_per_request=15)
+    def post(self):
+        order = dict()
+        order['tyres'] = []
+
+        for key, value in self.request.POST.iteritems():
+            try:
+                k = ndb.Key(urlsafe=key)
+                t = k.get()
+                order['tyres'].append(("Riepas ID: %(id)s \
+                                       Nosaukums: %(name)s \
+                                       Skaits: %(count)s" %
+                                       {'id': k.id(),
+                                        'name': t.brand + " " + t.model +
+                                        " " + t.size,
+                                        'count': value}))
+            except:
+                order[key] = value
+
+        from_addr = "info@bropro.lv"
+        to_addr = "nejeega@gmail.com"
+
+        try:
+            msg = mail.EmailMessage()
+            msg.sender = from_addr
+            msg.to = to_addr
+            msg.subject = "Pasutijums"
+            msg.html = order
+            msg.send()
+        except apiproxy_errors.OverQuotaError, message:
+            logging.error(message)
+
+        self.session['alert'] = u'Pasūtījums veiksmīgs! Ar Jums sazināsies \
+            mūsu pārstāvis!'
+        self.redirect('/grozs/dzest')
+
+
+class ClearCartHandler(Handler):
+    def get(self, key_str=None):
+        if key_str:
+            item_list = self.session.get('item_list')
+            item = next((x for x in item_list if x == key_str), None)
+            if item:
+                item_list.remove(item)
+            self.session['item_list'] = item_list
+            self.session['item_count'] -= 1
+        else:
+            self.session.pop('item_list')
+            self.session.pop('item_count')
+        self.redirect(self.request.referer)
 
 
 class RimHandler(Handler):
@@ -125,32 +253,32 @@ class SearchHandler(Handler):
         self.render("search.html", term=term)
 
 
-class SendMail(Handler):
+class MailHandler(Handler):
+    @rate_limit(seconds_per_request=15)
     def post(self):
-        user_name = self.request.get("name")
         user_info = self.request.get("cinfo")
-        # TODO: Add rate limiting
         message = self.request.get("message")
+        # TODO: Add rate limiting
         from_addr = "info@bropro.lv"
         to_addr = "nejeega@gmail.com"
 
         try:
             mail.send_mail(from_addr,
                            to_addr,
-                           'Test mail',
-                           'Vards: %(name)s \n Kontaktinfo: %(info)s\n \
-                           Zinja: %(message)s' % {'name': user_name,
-                                                  'info': user_info,
+                           'No BroPro lapas',
+                           'Personas kontaktinfo: %(info)s\n \
+                            Zina: %(message)s' % {'info': user_info,
                                                   'message': message})
+            self.session['alert'] = u'Ziņa nosūtīta veiksmīgi!'
+            self.redirect(self.request.referer)
         except apiproxy_errors.OverQuotaError, message:
             # Log the error.
             logging.error(message)
             # Redirect (?) and
-            # TODO: Display an informative message to the user.
-            self.write('The email could not be sent. '
-                       'Please try again later.')
+            self.session['alert'] = u'Ziņu neizdevās nosūtīt. \
+                Lūdzu, mēģiniet vēlāk.'
+            self.redirect(self.request.referer)
         # TODO: Implement redirect with success message.
-        self.write("Done?")
 
 
 class PopulateDB(Handler):
@@ -171,38 +299,38 @@ class PopulateDB(Handler):
                 e = entityName(**params)
                 e.put()
 
-        a = [["195/70 R15", "10", "R", "Firestone",
-              "MPS 530", "Ziemas", "50.00", 4],
-             ["195/65 R15", "9", "T", "Goodyear",
-              "MP 92", "Ziemas", "33.00", 3],
-             ["205/55 R16", "9", "H", "Michelin",
-              "MP 59", "Ziemas", "45.00", 3],
-             ["205/55 R16", "9", "T", "Falken",
-              "KW 22", "Ziemas", "50.00", 2],
+        a = [["205/55 R16", "91", "H", "StarFire",
+              "RS-C 2.0", "Ziemas", "50.00", 4],
+             ["235/75 R17", "9", "T", "Aeolus",
+              "HN804", "Ziemas", "33.00", 3],
+             ["255/75 R18", "9", "H", "Goodyear",
+              "Wrangler S4", "Ziemas", "45.00", 3],
+             ["255/55 R18", "9", "T", "Pirelli",
+              "Scorpion", "Ziemas", "50.00", 2],
              ["205/55 R16", "9", "T", "General",
               "Frigo 2", "Ziemas", "42.00", 1],
              ["235/75 R17", "13", "L", "Debica",
               "HN804", "Vissez.", "110.00", 0]]
 
-        b = [["255/55 R18", "10", "H", "Dunlop",
-              "Scop", "Vs.", "15.00", 3, "4.0"],
+        b = [["255/55 R18", "10", "H", "Dayton",
+              "DW510", "Vs.", "15.00", 3, "4.0"],
              ["220x508 R18", "8", "Q", "Bridgestone",
-              "MI", "Vissez.", "15.00", 0, "5.0"]]
+              "Wrangler Yada", "Vissez.", "15.00", 0, "5.0"]]
 
-        c = [["385/65 R22.5", "13", "L", "Infinity",
-              "M350", "V.", "290.0", 5, 'U'],
-             ["385/55 R22.5", "13", "L", "Hankook",
+        c = [["385/65 R22.5", "13", "L", "Antyre",
+              "TB1000", "V.", "290.0", 5, 'U'],
+             ["385/55 R22.5", "13", "L", "Yokohama",
               "HN809", "V.", "230.0", 0, "U"],
              ["385/65 R22.5", "13", "L", "Dayton",
               "HN805", "V.", "210.0", 6, "U"],
              ["385/65 R22.5", "13", "L", "BFGoodrich",
               "FH2", "V.", "250.0", 0, "U"],
-             ["315/80 R22.5", "13", "L", "Fulda",
-              "TB755", "V.", "175.0", 6, "U"],
+             ["315/80 R22.5", "13", "L", "Matador",
+              "Silent", "V.", "175.0", 6, "U"],
              ["315/80 R22.5", "13", "L", "Tigar",
               "HN 355", "V.", "220.0", 2, "F"],
-             ["315/70 R22.5", "13", "L", "Yokohama",
-              "RS03", "Vi.", "250.0", 2, "F"],
+             ["315/70 R22.5", "13", "L", "Kormoran",
+              "LHT", "Vi.", "250.0", 2, "F"],
              ["315/80 R22.5", "13", "L", "Nankang",
               "XD2", "V.", "160.0", 1, "U"],
              ["385/55 R22.5", "13", "J", "Kumho",
@@ -218,8 +346,8 @@ class PopulateDB(Handler):
               "Va", "25.00", 4, 'S', "4"],
              ["385/55 R22.5", "13", "L", "Pirelli",
               "HT1", "V.", "45.00", 6, 'U', "7"],
-             ["315/70 R22.5", "13", "L", "Kormoran",
-              "R6", "V", "35.00", 0, 'U', "11"],
+             ["315/70 R22.5", "13", "L", "Continental",
+              "Earthmover", "V", "35.00", 0, 'U', "11"],
              ["11 R22.5", "13", "L", "Kormoran",
               "DR1", "V", "45.00", 2, 'U', "5"]]
 
@@ -242,36 +370,53 @@ class PopulateDB(Handler):
         addEntries(TruckTyre, truckTyreDL)
         addEntries(UsedTruckTyre, usedTruckTyreDL)
 
-        self.redirect("/")
+        self.redirect(self.request.referer)
 
 
-class TyreHandler(Handler):
-    def get(self, kind=None, itemID=None):
+class Utils(Handler):
+    @staticmethod
+    def getKindFromPath(path):
+        tyrePaths = {'CarTyre': 'vieglo',
+                     'UsedCarTyre': 'vieglo/lietotas',
+                     'TruckTyre': 'kravas',
+                     'UsedTruckTyre': 'kravas/lietotas',
+                     'vieglo': CarTyre,
+                     'vieglo/jaunas': CarTyre,
+                     'vieglo/lietotas': UsedCarTyre,
+                     'kravas': TruckTyre,
+                     'kravas/jaunas': TruckTyre,
+                     'kravas/lietotas': UsedTruckTyre}
+        return tyrePaths[path]
 
+
+class TyresHandler(Handler):
+    def get(self, path=None):
         tyres = None
-
-        if kind:
-            if itemID:
-                key = ndb.Key(self.tyrePaths[kind], int(itemID))
-                tyres = [key.get()]
-                self.render("riepas.html", Tyres=tyres)
-                return
+        if path:
+            if path == 'lietotas':
+                tyres = list(UsedCarTyre.queryTyres()) + \
+                    list(UsedTruckTyre.queryTyres())
+            elif 'jaunas' or 'lietotas' in path:
+                tyres = list(Utils.getKindFromPath(path).queryTyres())
             else:
-                if kind == 'lietotas':
-                    tyres = list(UsedCarTyre.queryTyres()) + \
-                        list(UsedTruckTyre.queryTyres())
-                elif 'jaunas' or 'lietotas' in kind:
-                    tyres = list(self.tyrePaths[kind].queryTyres())
-                else:
-                    # feed the dada
-                    tyres = list(self.tyrePaths[kind].queryTyres()) + \
-                        list(self.tyrePaths[kind + '/lietotas'].queryTyres())
+                tyres = list(Utils.getKindFromPath(path).queryTyres()) + \
+                    list(Utils.getKindFromPath(path +
+                                               '/lietotas').queryTyres())
                 self.render("riepas.html", Tyres=tyres)
                 return
+        else:
+            tyres = list(CarTyre.queryTyres()) + list(UsedCarTyre.queryTyres()) + \
+                list(TruckTyre.queryTyres()) + list(UsedTruckTyre.queryTyres())
 
-        tyres = list(CarTyre.queryTyres()) + list(UsedCarTyre.queryTyres()) + \
-            list(TruckTyre.queryTyres()) + list(UsedTruckTyre.queryTyres())
         self.render("riepas.html", Tyres=tyres)
+
+
+class SingleTyreHandler(Handler):
+    def get(self, key_str=None):
+        key = ndb.Key(urlsafe=key_str)
+        if key:
+            tyre = key.get()
+        self.write(tyre)
 
 
 class EntriesHandler(Handler, blobstore_handlers.BlobstoreUploadHandler):
@@ -303,9 +448,10 @@ class EntriesHandler(Handler, blobstore_handlers.BlobstoreUploadHandler):
         tyre = CarTyre(**params)
 
         tyre.put()
-        self.redirect('')
+        self.redirect(self.request.referer)
 
 
+# Blobstore
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
         # 'file' is file upload field in the form
@@ -328,7 +474,7 @@ class ThumbnailHandler(blobstore_handlers.BlobstoreDownloadHandler):
 
         if blob_info:
             img = images.Image(blob_key=resource)
-            img.resize(width=80, height=100)
+            img.resize(width=100, height=100)
             thumbnail = img.execute_transforms(
                 output_encoding=images.JPEG)
 
@@ -341,21 +487,31 @@ class ThumbnailHandler(blobstore_handlers.BlobstoreDownloadHandler):
         self.error(404)
 
 
+class NoiceHandler(Handler):
+    def get(self):
+        self.write("NOICE!")
+
+
 app = webapp2.WSGIApplication([
+    ('/admin/.*', NoiceHandler),
     ('/db', PopulateDB),
     ('/add', EntriesHandler),
     ('/diski', RimHandler),
     ('/meklet', SearchHandler),
     ('/par', AboutHandler),
-    ('/l', SendMail),
-    ('/riepas(?:/)?(?:([a-zA-Z]+(?:(?:/)?[a-zA-Z]+)?)?(?:/)?([0-9]+)?)?',
-     TyreHandler),
+    ('/l', MailHandler),
+    ('/riepa/(.*)?', SingleTyreHandler),
+    ('/riepas(?:/)?(?:([a-zA-Z]+(?:(?:/)?[a-zA-Z]+)?)?(?:/)?)?',
+     TyresHandler),
     ('/serviss(?:/)?([a-zA-Z]+)?(?:/)?',
      ServiceHandler),
     ('/upload', UploadHandler),
     ('/serve/([^/]+)?', ServeHandler),
     ('/serve/th/([^/]+)?', ThumbnailHandler),
-    ('/.*', MainHandler)
-], debug=True)
+    ('/pirkt.*', AddToCartHandler),
+    ('/grozs', ShowCartHandler),
+    ('/grozs/dzest(?:/)?(.*)?', ClearCartHandler),
+    ('/.*', MainHandler),
+], config=config, debug=True)
 
 # TODO: Implement feature to find product by car model.
